95716d383c65b7396392cfc9adfb3b9f9032acc0
Taqdees Mehdi <taqdeesm99@gmail.com>
2025-08-07 13:05:55 +0500

    adding callbacks

--- auth.ts
+++ auth.ts
@@ -3,6 +3,14 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      return session;
+    },
+    async jwt({ token }) {
+      return token;
+    },
+  },
   adapter: DrizzleAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
