66444ec096836d8766a3b5edfa153fd084380893
Taqdees Mehdi <taqdeesm99@gmail.com>
2025-08-07 15:17:49 +0500

    Call back completed

--- auth.ts
+++ auth.ts
@@ -1,13 +1,34 @@
 import { db } from "@/db";
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import authConfig from "@/auth.config";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { getUserById } from "./data/user";
+export type ExtendedUser = DefaultSession["user"] & {
+  role: "ADMIN" | "USER";
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
 export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks: {
     async session({ token, session }) {
+      console.log({ sessionToken: token });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as "ADMIN" | "USER";
+      }
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
       return token;
     },
   },
